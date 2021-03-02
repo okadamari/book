fd3d9010f4d78c188536afb8f845afa42afeae8a
okada <47173004+okadamari@users.noreply.github.com>
2021-03-03 06:55:55 +0900

    use  material-ui for the BookRow component. and remove bookItem component

--- src/layouts/App.tsx
+++ src/layouts/App.tsx
@@ -2,10 +2,6 @@ import React, { useState, useEffect } from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Button from '@material-ui/core/Button';
 import CameraIcon from '@material-ui/icons/PhotoCamera';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Grid from '@material-ui/core/Grid';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -156,31 +152,7 @@ export default function Album(): JSX.Element {
         <Container className={classes.cardGrid} maxWidth="md">
           <Grid container spacing={4}>
             {cards.map((card) => (
-              <Grid item key={card} xs={12} sm={6} md={4}>
-                <Card className={classes.card}>
-                  <CardMedia
-                    className={classes.cardMedia}
-                    image="https://source.unsplash.com/random"
-                    title="Image title"
-                  />
-                  <CardContent className={classes.cardContent}>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      Heading
-                    </Typography>
-                    <Typography>
-                      This is a media card. You can use this section to describe the content.
-                    </Typography>
-                  </CardContent>
-                  <CardActions>
-                    <Button size="small" color="primary">
-                      View
-                    </Button>
-                    <Button size="small" color="primary">
-                      Edit
-                    </Button>
-                  </CardActions>
-                </Card>
-              </Grid>
+              <Grid item key={card} xs={12} sm={6} md={4} />
             ))}
           </Grid>
         </Container>
--- src/layouts/BookRow.tsx
+++ src/layouts/BookRow.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
 import { BookToRead } from '../types/BookToRead';
-import BookItem from './BookItem';
 
 type BookRowProps = {
   book: BookToRead;
@@ -8,9 +14,31 @@ type BookRowProps = {
   onDelete: (id: number) => void;
 };
 
+const useStyles = makeStyles((theme) => ({
+  icon: {
+    marginRight: theme.spacing(2),
+  },
+  cardGrid: {
+    paddingTop: theme.spacing(8),
+    paddingBottom: theme.spacing(8),
+  },
+  card: {
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  cardMedia: {
+    paddingTop: '56.25%', // 16:9
+  },
+  cardContent: {
+    flexGrow: 1,
+  },
+}));
+
 const BookRow = (props: BookRowProps): JSX.Element => {
   const { book: propsBook } = props;
   const { book, memo } = propsBook;
+  const classes = useStyles();
 
   const handleMemoChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     props.onMemoChange(props.book.id, e.target.value);
@@ -21,26 +49,39 @@ const BookRow = (props: BookRowProps): JSX.Element => {
   };
 
   return (
-    <div className="book-item">
-      <BookItem
-        title={book.title}
-        authors={book.authors}
-        thumbnail={book.thumbnail}
+    <Card className={classes.card}>
+      <CardMedia
+        className={classes.cardMedia}
+        image={book.thumbnail}
+        title={`${book.title}のサムネイル`}
       />
-      <input
-        type="text"
-        className="memo"
-        value={memo}
-        onChange={handleMemoChange}
-      />
-      <button
-        type="button"
-        className="delete-row"
-        onClick={handleDeleteClick}
-      >
-        削除
-      </button>
-    </div>
+      <CardContent className={classes.cardContent}>
+        <Typography gutterBottom variant="h5" component="h2">
+          {book.title}
+        </Typography>
+        <Typography>
+          {book.authors}
+        </Typography>
+        <input
+          type="text"
+          className="memo"
+          value={memo}
+          onChange={handleMemoChange}
+        />
+      </CardContent>
+      <CardActions>
+        <Button
+          size="small"
+          color="primary"
+          onClick={handleDeleteClick}
+        >
+          削除
+        </Button>
+        <Button size="small" color="primary">
+          Edit
+        </Button>
+      </CardActions>
+    </Card>
   );
 };
 
